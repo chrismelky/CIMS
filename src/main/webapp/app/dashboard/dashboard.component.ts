b3c5c59c99071ad9a621db631e0cc71e6d66088d
chrismelky <chrismelky05@gmail.com>
2020-02-29 17:36:22 +0300

    Revert "add dashboard"
    This reverts commit 2d5f9f251f6a0e196e688df3490f76504f3f4f7b.

--- src/main/webapp/app/dashboard/dashboard.component.ts
+++ src/main/webapp/app/dashboard/dashboard.component.ts
@@ -6,75 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-  dataSource: Object;
-  chartConfig: Object;
-
-  constructor() {
-    this.chartConfig = {
-      width: '800',
-      height: '400',
-      type: 'mscolumn2d',
-      dataFormat: 'json'
-    };
-
-    this.dataSource = {
-      chart: {
-        caption: 'Ahadi za Michango na Makusanyo',
-        subcaption: '2020',
-        xaxisname: 'Aina ya Machango',
-        yaxisname: 'Amount in Tshs',
-        formatnumberscale: '1',
-        plottooltext: '<b>$dataValue</b> apps were available on <b>$seriesName</b> in $label',
-        theme: 'fusion',
-        drawcrossline: '1'
-      },
-      categories: [
-        {
-          category: [
-            {
-              label: 'Ujenzi'
-            },
-            {
-              label: 'Baasha'
-            },
-            {
-              label: 'Zaka'
-            }
-          ]
-        }
-      ],
-      dataset: [
-        {
-          seriesname: 'Ahadi',
-          data: [
-            {
-              value: '125000'
-            },
-            {
-              value: '300000'
-            },
-            {
-              value: '480000'
-            }
-          ]
-        },
-        {
-          seriesname: 'Makusanyo',
-          data: [
-            {
-              value: '70000'
-            },
-            {
-              value: '150000'
-            },
-            {
-              value: '350000'
-            }
-          ]
-        }
-      ]
-    };
-  }
+  constructor() {}
 
   ngOnInit() {}
 }
--- src/main/webapp/app/dashboard/dashboard.module.ts
+++ src/main/webapp/app/dashboard/dashboard.module.ts
@@ -3,15 +3,9 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
-import { FusionChartsModule } from 'angular-fusioncharts';
-
-import * as FusionCharts from 'fusioncharts';
-import * as Charts from 'fusioncharts/fusioncharts.charts';
-import * as FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
-FusionChartsModule.fcRoot(FusionCharts, Charts, FusionTheme);
 
 @NgModule({
   declarations: [DashboardComponent],
-  imports: [CommonModule, DashboardRoutingModule, FusionChartsModule]
+  imports: [CommonModule, DashboardRoutingModule]
 })
 export class DashboardModule {}
